cf7962fe92bdee79d45494eb884012fff0564ced
Lautaro Petaccio <lausuper@gmail.com>
2021-12-03 13:28:16 -0300

    feat: Create base repository

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  preset: "ts-jest",
+  collectCoverageFrom: ["src/**/*.{js,ts}", "!src/**/*.d.ts"],
+  testMatch: ["<rootDir>/src/**/?(*.)spec.{js,ts}"],
+  testEnvironment: "node",
+};
