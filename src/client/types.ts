913a8fb3f9cea5bab24dabffb9f51a1c6bb4678f
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2022-03-15 15:44:38 -0300

    feat: Add getNFT/s methods

--- src/client/types.ts
+++ src/client/types.ts
@@ -3,3 +3,37 @@ export type ServerResponse<T> = {
   ok: boolean
   error?: string
 }
+
+/**
+ * NFT types required by the client
+ * They have been copied from the builder server.
+ * TODO: Find a way to abstract these types someplace else to avoid this nasty repetition
+ */
+
+export type NFT = {
+  tokenId: string
+  name: string
+  thumbnail: string
+  contract: {
+    name: string
+    address: string
+  }
+}
+
+export type GetNFTsParams = {
+  owner?: string
+  first?: number
+  skip?: number
+  cursor?: string
+}
+
+export type GetNFTsResponse = {
+  next: string | null
+  previous: string | null
+  nfts: NFT[]
+}
+
+export type GetNFTParams = {
+  contractAddress: string
+  tokenId: string
+}
